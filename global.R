f75023592d632276a84982762e9c28aeafdc842a
Koki Ando <basket.koki.bowl@gmail.com>
2018-09-25 11:50:38 +1000

    global setting

--- global.R
+++ global.R
@@ -1,1 +1,19 @@
+## Global Script
+
+# Package loading
 library(tidyverse)
+library(shiny)
+library(DT)
+library(data.table)
+library(bit64)
+library(maps)
+library(plotly)
+library(shinyjs)
+
+
+# Data setting
+gtd <- fread("https://s3-ap-southeast-2.amazonaws.com/globalterrorismdataset/global_terrorism_dataset.csv", data.table = FALSE)
+gtd$iyear <- as.numeric(gtd$iyear)
+gtd$imonth <- as.numeric(gtd$imonth)
+gtd$iday <- as.numeric(gtd$iday)
+gtd$date <- as.Date(gtd$date)
